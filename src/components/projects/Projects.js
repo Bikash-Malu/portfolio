17188b17e73c5c68f1484a0ec178032dcd4775d7
bikashmalu <bikashmalu1@gmail.com>
2024-03-30 17:08:35 +0530

    add github link

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -20,17 +20,20 @@ const Projects = () => {
           title="Footwear Page"
           des="  Footwear Hub is an e-commerce platform dedicated to providing users with a wide range of footwear options. It is only owner side webpage where he create the bill and make the orders. Tech Stack:-React jS, Node JS ,Express JS, Ant UI, MongoDB ."
           src={projectOne}
+          link="https://github.com/Bikash-Malu/footwear_frontend"
         />
         <ProjectsCard
           title="A2Z E-commerce Website"
           des=" Multi-Venvor is a comprehensive multi-vendor e-commerce platform that connects buyers and sellers, facilitating seamless transactions for a wide range of products. Built with Spring Boot as the backend framework and React as the frontend library. Tech Stack:-ReactJS, Spring Boot, Mysql."
           src={projectTwo}
+          link="https://github.com/Bikash-Malu/A2Z_Store_frontend"
         />
         <ProjectsCard
           title="Student Subject Regd."
           des="a student subject registration webpage using Spring Boot for the backend and React for the frontend involves multiple steps Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint laudantium facilis excepturi eum, eos nemo! Harum dolor nemo facilis inventore!"
         
           src={projectThree}
+          link="https://github.com/Bikash-Malu/ecommerce_frontend"
         />
         
        
